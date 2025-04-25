a2d92d1f109d2ddc20869d886fc031291662327c
Ankit Kumar Singh <136215049+ankitmeofficial@users.noreply.github.com>
2025-04-25 09:35:53 +0530

    updated directory

--- backend/routes/scanRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { scanWebsite, generateReport } = require('../controllers/scanController');
-const router = express.Router();
-
-router.post('/', scanWebsite);
-router.post('/download-report', generateReport);
-
-module.exports = router;
